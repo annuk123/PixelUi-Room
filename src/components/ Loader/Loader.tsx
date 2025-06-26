178f79b069ca2a060caf486e0282f3ab0d4a528d
Annu Kumari <anuk35168@gmail.com>
2025-06-26 22:18:43 +0530

    loading page renamed

--- src/components/ Loader/Loader.tsx
+++ src/components/ Loader/Loader.tsx
@@ -45,7 +45,8 @@ export default function Loader({
           className="text-center leading-tight space-y-2"
         >
           <h1 className="text-white text-4xl md:text-6xl font-extrabold tracking-tight">
-            Pixel<span className="text-blue-500">Craft</span> Studio
+            Pixel<span className="text-blue-500">UI</span> Roo
+            <span className="text-blue-500">m</span> 
           </h1>
           <p className="text-blue-300 text-lg md:text-xl">
             Welcome to Creativity
