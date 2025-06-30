53d0168ad07a1ea7b937911b582bacc0d99f02c9
Annu Kumari <anuk35168@gmail.com>
2025-06-30 13:54:13 +0530

    vercel Analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ConvexClientProvider } from "@/components/provider/provider";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
            <ConvexClientProvider>
         {children}
         </ConvexClientProvider>
+        <Analytics />
       </body>
     </html>
   );
